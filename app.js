2ff5861d2b8f55f83d5306f1818eb41fbd0481a8
arif <moarifur@gmail.com>
2021-06-21 23:35:07 +0600

    JS training: 2nd class

---
+++ app.js
@@ -0,0 +1,24 @@
+// console.log('yo')
+
+/* Loop -
+
+1. while()
+2. for()
+3. do..while()
+4. for..of()
+5. for..in()
+7. forEach()
+
+*/ 
+
+let i = 1
+let sum = 0
+while (i<6) {
+    sum += i
+    console.log(`When ${i}, the summation is: ${sum}`)
+    i++
+}
+console.log(`When ${i}, we are out of loop`)
+// console.log(sum)
+// console.log('The summation from 1 to 5 is: ' + sum + ' So you have got your answer, right?')
+console.log(`The summation from 1 to 5 is: ${sum}. So you have got your answer, right?`)
