fde7d97d1bc925025c5d3c850857738bcd493743
arif <moarifur@gmail.com>
2021-06-21 23:52:08 +0600

    JS training: 2nd class - While and for loop details

--- app.js
+++ app.js
@@ -11,14 +11,30 @@
 
 */ 
 
+// ---------------------- 1. while() loop ------------------------------
+console.log(`--------------- 1. while() loop ---------------`)
 let i = 1
 let sum = 0
-while (i<6) {
+let flag = true
+while (flag) {
+// while (i<6) {
     sum += i
     console.log(`When ${i}, the summation is: ${sum}`)
     i++
+    if(i === 6){
+        flag = false
+        break
+    }
+    
 }
 console.log(`When ${i}, we are out of loop`)
-// console.log(sum)
-// console.log('The summation from 1 to 5 is: ' + sum + ' So you have got your answer, right?')
+console.log(`The summation from 1 to 5 is: ${sum}. So you have got your answer, right?`)
+
+// ------------------------- 2. for() -----------------------------------
+console.log(`--------------- 2. for() loop ---------------`)
+for (let i = 1; i < 6; i++) {
+    sum += i
+    console.log(`When ${i}, the summation is: ${sum}`)
+}
+console.log(`When ${i}, we are out of loop`)
 console.log(`The summation from 1 to 5 is: ${sum}. So you have got your answer, right?`)
