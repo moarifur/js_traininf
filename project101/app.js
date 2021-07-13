75d19e5a030a020bf2e08de5dc15c060bfb3e6fc
arif <moarifur@gmail.com>
2021-07-13 21:07:22 +0600

    Milestone01: Baby Project | Design with HTML & CSS | Development with JS

---
+++ project101/app.js
@@ -0,0 +1,22 @@
+console.log('Connected!')
+
+/*
+    Variable
+    Conditionals: if-else
+    Iteration: Loop: for...in / for...of / forEach()
+    Function: define, call
+    DOM: Manipulation
+
+*/ 
+
+// let button = document.querySelector('#clickMe')
+// button.addEventListener('click', (e) =>{
+//     alert('Ouch!')
+// })
+
+
+/*
+    Baby Project
+        - Design Baby with HTML & CSS and add eventListener for interactivity
+        - Design and development Baby With JS
+*/ 
